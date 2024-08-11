d0284fba0bb54be3d024e68f12c186ac686883f5
Vivek Pokharankar <vivek00088888@gmal.com>
2024-08-12 00:37:32 +0530

    recent commit

--- Intersection/src/Main.java
+++ Intersection/src/Main.java
@@ -1,18 +1,40 @@
-public class Main {
-    public static void main(String[] args) {
-        int[] arr = {4,5,6,2,4,99,4};
+class Solution {
+    public int[] intersection(int[] nums1, int[] nums2) {
+        // int n = (nums1.length<nums2.length) ? nums1.length : nums2.length;
+        // int[] arr = new int[n];
+        // for(int i =0;i<nums1.length;i++) {
+        //     for(int j =0;j<nums2.length;j++) {
+        //         if(arr.length<n && arr[i]!=  nums2[j] && nums1[i] == nums2[j])
+        //         arr[i] = nums1[i];
+
+
+        //     }
+        // }
+        // return arr;
+
         int[] count = new int[1001];
-        for(int num : arr) {
-            System.out.println(count[num]);
+        int n = (nums1.length>nums2.length) ? nums1.length : nums2.length;
+
+        int j=0;
+        int countinter = 0;
+        for(int num : nums1) {
+            count[num] =1;
         }
+        for(int num : nums2) {
+            if(count[num] ==1){
+                count[num] =2;
+                countinter++;
+            }
 
+        }
+        int[] arr = new int [countinter];
+        for(int i=0;i<count.length;i++) {
+            if(count[i]==2) {
+                arr[j] = i;
+                j++;
+            }
+
+        }
+        return arr;
     }
-
-//    public static int[] inter(int[] arr1, int arr2) {
-//        int[] count = new int[1001];
-//        for(int num: arr1) {
-//
-//
-//        }
-//    }
 }
